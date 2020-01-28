da74da12180c997455529a063d247b466fd2d47d
Khaoula <55798632+AlKhaoula02@users.noreply.github.com>
2020-01-28 15:14:21 +0100

    MAJ

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,29 @@ import '../css/app.css';
 // import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+    
+// var ny = [40.741895, -73.989308];
+// // création de la map
+// var map = L.map('map').setView(ny, 6);
+
+// // création du calque images
+// L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+// 	maxZoom: 20
+// }).addTo(map);
+
+// // ajout d'un markeur
+// var marker = L.marker(ny).addTo(map);
+
+// // ajout d'un popup
+// marker.bindPopup('<h3> New York, USA. </h3>');
+
